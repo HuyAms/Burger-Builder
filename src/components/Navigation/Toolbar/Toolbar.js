240ef7be12b03a429837f2f204b907f9a83a4d6c
HuyTrinh <dinhhuyams@gmail.com>
2018-03-05 23:43:55 +0200

    add drawertoggle

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NagvigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-      <div>MEMU</div>
+      <DrawerToggle clicked={props.drawerToggleClicked}/>
       <div className={classes.Logo}>
         <Logo/>
       </div>
