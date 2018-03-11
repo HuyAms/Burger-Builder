e8a6df593adb31b9fc6320723812a0fbba10a452
HuyTrinh <dinhhuyams@gmail.com>
2018-03-11 12:35:08 +0200

    sending a Post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -81,7 +82,24 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinuehandler = () => {
-    alert('You continue!');
+    //alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Huy Trinh',
+        address: {
+          street: 'Kilo',
+          zipCode: '02645',
+          country: 'Finland'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fatest'
+    }
+    axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
   };
 
   render() {
