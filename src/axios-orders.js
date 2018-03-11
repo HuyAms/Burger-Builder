a2d2ae8a77fa31438b282d947d1ca2d14f451ad8
HuyTrinh <dinhhuyams@gmail.com>
2018-03-11 15:48:41 +0200

    handle error globally

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://burger-builder-5a0c7.firebaseio.com/'
+  baseURL: 'https://burger-builder-5a0c7.firebaseio.comf/'
 });
 
 export default instance;
