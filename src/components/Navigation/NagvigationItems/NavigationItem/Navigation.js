1fd2331e083aaf449eb98c71f4082d617148f732
HuyTrinh <dinhhuyams@gmail.com>
2018-03-14 21:17:42 +0200

    implement navigation links

--- src/components/Navigation/NagvigationItems/NavigationItem/Navigation.js
+++ src/components/Navigation/NagvigationItems/NavigationItem/Navigation.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { NavLink} from 'react-router-dom';
 import classes from './Navigation.css';
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-      <a
-          herf={props.link}
-          className={props.active ? classes.active : null}
-      >{props.children}</a>
+      <NavLink
+          to={props.link}
+          exact={props.exact}
+          activeClassName={classes.active}
+      >{props.children}</NavLink>
     </li>
 );
 
