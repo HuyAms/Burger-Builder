5652b47ffbb994bf8973616e115d3852c8f2cb70
HuyTrinh <dinhhuyams@gmail.com>
2018-05-14 13:47:18 +0300

    add signin/signup

---
+++ src/store/actions/auth.js
@@ -0,0 +1,47 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (authData) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  };
+};
+
+export const authFailed = (error) => {
+  return {
+    type: actionTypes.AUTH_FAILED,
+    error: error,
+  };
+};
+
+export const auth = (email, password, isSignUp) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken	: true,
+    };
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCiceZFCwXVkueRhM0ulEU_1tHa8TIEEDw';
+    if (!isSignUp) {
+      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCiceZFCwXVkueRhM0ulEU_1tHa8TIEEDw';
+    }
+    axios.post(url,
+        authData)
+        .then(response => {
+          console.log(response);
+          dispatch(authSuccess(response.data))
+        })
+        .catch(err => {
+          console.log(err);
+          dispatch(authFailed(err));
+    })
+  };
+};
